258af8418e14e3da59e62bc798df9b98000d148f
Heber Arratia <heber.im87@gmail.com>
2017-10-09 02:49:16 -0300

    add website

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$('.main-carousel').flickity({
+		  // options
+		autoPlay: 3000,
+		wrapAround: true
+	});
+    
+    $(window).scroll(function() {
+	    if($(window).scrollTop() > 400){
+	    	$('#menu').css({'opacity' : '1'});
+	    } else {
+	    	$('#menu').css({'opacity' : '0'});
+	    }
+    });
+
+   $("#modal").iziModal({
+	  headerColor: '#FF5766', //ヘッダー部分の色
+	  width: '50%', //横幅
+	  overlayColor: 'rgba(0, 0, 0, 0.5)', //モーダルの背景色
+	  transitionIn: 'fadeInUp', //表示される時のアニメーション
+	  transitionOut: 'fadeOutDown' //非表示になる時のアニメーション
+	});
+
+   $(document).on('click', '.trigger', function (event) {
+    	event.preventDefault();
+    	$('#modal').iziModal('open');
+	});
+});
